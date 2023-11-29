482f844a30533fef2e3a65171d34f41349cebee8
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-11-30 00:20:28 +0200

    Initial Commit

---
+++ DeathNote/DeathNote.h
@@ -0,0 +1,18 @@
+//
+//  DeathNote.h
+//  DeathNote
+//
+//  Created by Danylo Litvinchuk on 30.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeathNote.
+FOUNDATION_EXPORT double DeathNoteVersionNumber;
+
+//! Project version string for DeathNote.
+FOUNDATION_EXPORT const unsigned char DeathNoteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeathNote/PublicHeader.h>
+
+
